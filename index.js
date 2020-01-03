3a4dd54b7c2bca364a02124216e21a6ff64fecda
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2020-01-03 22:18:18 +0100

    index.js file for express API.

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const compression = require("compression");
+const rateLimit = require("express-rate-limit");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+
+// const dbFunctions = require("./functions/dbfunctions");
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(compression());
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 45 // 45 requests,
+});
+app.use(limiter);
+
+app.use(cors());
+
+app.get("/search/", dbFunctions.getWorks);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}/`);
+});
