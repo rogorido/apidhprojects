fea4fc3018125260914885f099e0e714747d6536
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2020-01-04 07:51:28 +0100

    Using env variables for connecting to the db.

--- populatedb.js
+++ populatedb.js
@@ -2,12 +2,7 @@ const { Pool } = require("pg");
 const projects = require("./projects.json");
 require("dotenv").config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: `dhprojects`,
-  port: `5432`
-});
+const pool = new Pool({});
 
 for (var i = 0; i < projects.length; i++) {
   const sqlstring =
